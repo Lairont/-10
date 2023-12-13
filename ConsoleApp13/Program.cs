0eedb194dae2ca4ab3152ff40d59217c961b1d79
Никита <153456515+Lairont@users.noreply.github.com>
2023-12-14 02:59:36 +0300

    Add files via upload

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание 1
+            //Console.Write("День недели: ");
+            //byte day = byte.Parse(Console.ReadLine());
+            //switch (day)
+            //{
+            //    case 1: Console.WriteLine("Понедельник"); break;
+            //    case 2: Console.WriteLine("Вторник"); break;
+            //    case 3: Console.WriteLine("Среда"); break;
+            //    case 4: Console.WriteLine("Четверг"); break;
+            //    case 5: Console.WriteLine("Пятница"); break;
+            //    case 6: Console.WriteLine("Суббота"); break;
+            //    case 7: Console.WriteLine("Воскресенье"); break;
+            //    default: Console.WriteLine("Ошибка"); break;
+            //}
+            //Console.ReadLine();
+            //задание 2
+            //Console.Write("Месяц: ");
+            //byte month = byte.Parse(Console.ReadLine());
+            //switch (month)
+            //{
+            //    case 1: Console.WriteLine("Январь"); break;
+            //    case 2: Console.WriteLine("Февраль"); break;
+            //    case 3: Console.WriteLine("Март"); break;
+            //    case 4: Console.WriteLine("Апрель"); break;
+            //    case 5: Console.WriteLine("Май"); break;
+            //    case 6: Console.WriteLine("Июнь"); break;
+            //    case 7: Console.WriteLine("Июль"); break;
+            //    case 8: Console.WriteLine("Август"); break;
+            //    case 9: Console.WriteLine("Сентябрь"); break;
+            //    case 10: Console.WriteLine("Октябрь"); break;
+            //    case 11: Console.WriteLine("Ноябрь"); break;
+            //    case 12: Console.WriteLine("Декабрь"); break;
+            //    default: Console.WriteLine("Ошибка"); break;
+            //}
+            //Console.ReadLine();
+            //задание 3
+            //Console.Write("Месяц: ");
+            //byte month = byte.Parse(Console.ReadLine());
+            //switch (month)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2: Console.WriteLine("Зима"); break;
+            //    case 6:
+            //    case 7:
+            //    case 8: Console.WriteLine("Лето"); break;
+            //    case 3:
+            //    case 4:
+            //    case 5: Console.WriteLine("Весна"); break;
+            //    case 9:
+            //    case 10:
+            //    case 11: Console.WriteLine("Осень"); break;
+            //    default: Console.WriteLine("Ошибка"); break;
+            //}
+            //Console.ReadLine();
+            //задание 4
+            //Console.Write("Букву: ");
+            //char b = char.Parse(Console.ReadLine());
+            //switch (b)
+            //{
+            //    case 'ф':
+            //    case 'Ф': Console.WriteLine("Физика"); break;
+            //    case 'м':
+            //    case 'М': Console.WriteLine("Математика"); break;
+            //    case 'г':
+            //    case 'Г': Console.WriteLine("География"); break;
+            //    case 'и':
+            //    case 'И': Console.WriteLine("История"); break;
+            //    case 'б':
+            //    case 'Б': Console.WriteLine("Биология"); break;
+            //    default: Console.WriteLine("Ошибка"); break;
+            //}
+            //Console.ReadLine();
+
+        }
+    }
+}
